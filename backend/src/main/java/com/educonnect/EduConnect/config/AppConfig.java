e7b08aa5819471f0d62791b424fe768e0ba06d1e
w1ze55 <cacauezin@gmail.com>
2025-10-07 20:28:19 -0300

    fix: recados

--- backend/src/main/java/com/educonnect/EduConnect/config/AppConfig.java
+++ backend/src/main/java/com/educonnect/EduConnect/config/AppConfig.java
@@ -1,6 +1,9 @@
 package com.educonnect.EduConnect.config;
 
+import com.educonnect.EduConnect.dto.RecadoDTO;
+import com.educonnect.EduConnect.model.Recado;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +12,19 @@ public class AppConfig {
     
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        
+        // Configurar mapeamento de Recado para RecadoDTO
+        modelMapper.createTypeMap(Recado.class, RecadoDTO.class)
+            .addMappings(mapper -> {
+                mapper.skip(RecadoDTO::setRemetente);
+                mapper.skip(RecadoDTO::setRemetenteId);
+                mapper.skip(RecadoDTO::setLido);
+                mapper.skip(RecadoDTO::setDataLeitura);
+            });
+        
+        return modelMapper;
     }
 }
 
